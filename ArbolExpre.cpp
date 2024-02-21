28f74683266e7e4bb60f9424df26f0d28776eafc
agtalvi <94943833+agtalvi@users.noreply.github.com>
2024-02-21 14:38:48 -0300

    cambios ArbolExpre

--- ArbolExpre.cpp
+++ ArbolExpre.cpp
@@ -1,71 +1,84 @@
 #include "ArbolExpre.h"
 
+typedef struct nodoA
+{
+    ValorNodo info;
+    nodoA *hizq;
+    nodoA *hder;
+} NodoA;
 
-void crearArbol (ArbolExpre &a) {
+typedef NodoA *ArbolExpre;
 
-a = NULL;
+void crearArbol(ArbolExpre &a)
+{
 
+    a = NULL;
 }
 
-boolean ArbolEsVacio (ArbolExpre a) {
-boolean es;
+boolean ArbolEsVacio(ArbolExpre a)
+{
+    boolean es;
 
-if (a == NULL) {
-    es = TRUE;
-}
-else {
-    es = FALSE;
-}
-return es;
-}
-
-ValorNodo darRaiz (ArbolExpre a) {
-return a->info;
-
-}
-
-ArbolExpre HijoIzq (ArbolExpre a) {
-
-return a->hizq;
-
-}
-
-
-ArbolExpre HijoDer (ArbolExpre a) {
-
-return a->hder;
-}
-// Solo sirve para insertar desde archivo porque tiene que venir con el índice seteado, no sirve si el índice no está definido
-
-void insertarValorEnOrden (ValorNodo valor, ArbolExpre &a) {
-
-if (a == NULL) {
-    a = new NodoA;
-    a->info = valor;
-    a->hizq = NULL;
-    a->hder = NULL;
+    if (a == NULL)
+    {
+        es = TRUE;
     }
-    else {
-        if (darIndiceNodoArbol(valor)< darIndiceNodoArbol(a->info)) {
-            insertarValorEnOrden (valor, a->hizq);
-        }
-        else {
-            insertarValorEnOrden (valor, a->hder);
-        }
+    else
+    {
+        es = FALSE;
     }
-
+    return es;
 }
 
-void insertarValorArbol (boolean v, ArbolExpre &a) {
+ValorNodo darRaiz(ArbolExpre a)
+{
+    return a->info;
+}
+
+ArbolExpre HijoIzq(ArbolExpre a)
+{
+
+    return a->hizq;
+}
+
+ArbolExpre HijoDer(ArbolExpre a)
+{
+
+    return a->hder;
+}
+// Solo sirve para insertar desde archivo porque tiene que venir con el ï¿½ndice seteado, no sirve si el ï¿½ndice no estï¿½ definido
+
+void insertarValorEnOrden(ValorNodo valor, ArbolExpre &a)
+{
+
+    if (a == NULL)
+    {
+        a = new NodoA;
+        a->info = valor;
+        a->hizq = NULL;
+        a->hder = NULL;
+    }
+    else
+    {
+        if (darIndiceNodoArbol(valor) < darIndiceNodoArbol(a->info))
+        {
+            insertarValorEnOrden(valor, a->hizq);
+        }
+        else
+        {
+            insertarValorEnOrden(valor, a->hder);
+        }
+    }
+}
+
+void insertarValorArbol(boolean v, ArbolExpre &a)
+{
 
     a = new NodoA;
     insertarValorNodo(v, a->info);
     a->hizq = NULL;
     a->hder = NULL;
-    }
-
-
-
+}
 
 void liberarMemoriaArbol(ArbolExpre &a)
 {
@@ -77,100 +90,99 @@ void liberarMemoriaArbol(ArbolExpre &a)
     }
 }
 
-
-void desplegarNodosArbolOrden (ArbolExpre a) {
- if (a != NULL) {
-    desplegarNodosArbolOrden (a->hizq); // Se rompe acá, no llega a hacerlo cuando viene de la lista, pero si lo ejecuto solo en el árbol funciona
-    desplegarDatoNodoArbol (a->info);
-    desplegarNodosArbolOrden (a->hder);
- }
+void desplegarNodosArbolOrden(ArbolExpre a)
+{
+    if (a != NULL)
+    {
+        desplegarNodosArbolOrden(a->hizq); // Se rompe acï¿½, no llega a hacerlo cuando viene de la lista, pero si lo ejecuto solo en el ï¿½rbol funciona
+        desplegarDatoNodoArbol(a->info);
+        desplegarNodosArbolOrden(a->hder);
+    }
 }
 
-
-
-//boolean existe (ABB a, long int cedula) {
-//if (a == NULL) {
-//    return FALSE;
-//}
-//else {
-//    if (cedula == seleccionarCedulaPaciente(a->info)) {
-//        return TRUE;
-//    }
-//    else {
-//            if (cedula < seleccionarCedulaPaciente(a->info)) {
-//                return existe (a->hizq, cedula);
-//            }
-//            else {
-//                return existe (a->hder, cedula);
-//            }
-//    }
-//}
+// boolean existe (ABB a, long int cedula) {
+// if (a == NULL) {
+//     return FALSE;
+// }
+// else {
+//     if (cedula == seleccionarCedulaPaciente(a->info)) {
+//         return TRUE;
+//     }
+//     else {
+//             if (cedula < seleccionarCedulaPaciente(a->info)) {
+//                 return existe (a->hizq, cedula);
+//             }
+//             else {
+//                 return existe (a->hder, cedula);
+//             }
+//     }
+// }
 //
 //
-//}
+// }
 //
-//paciente Minimo (ABB a) {
-//    if (a -> hizq == NULL)   {
-//            return (a->info);
-//    }
-//    else {
-//         return Minimo (a -> hizq); }
+// paciente Minimo (ABB a) {
+//     if (a -> hizq == NULL)   {
+//             return (a->info);
+//     }
+//     else {
+//          return Minimo (a -> hizq); }
 //
-//    }
+//     }
 //
-//void Borrar_Minimo (ABB &a) {
-//    ABB aux;
-//    if (a -> hizq == NULL) {
-//            aux = a -> hder;
-//            delete a;
-//            a = aux;
-//    }
-//    else
-//    {
-//        Borrar_Minimo (a -> hizq);
-//    }
-//}
+// void Borrar_Minimo (ABB &a) {
+//     ABB aux;
+//     if (a -> hizq == NULL) {
+//             aux = a -> hder;
+//             delete a;
+//             a = aux;
+//     }
+//     else
+//     {
+//         Borrar_Minimo (a -> hizq);
+//     }
+// }
 //
-//void eliminarConCedula (long int cedula, ABB &a) {
+// void eliminarConCedula (long int cedula, ABB &a) {
 //
-//    ABB aux;
-//    if (cedula == seleccionarCedulaPaciente(a -> info)) {
-//        if (a -> hder == NULL) {
-//            aux = a -> hizq;
-//            delete a;
-//            a = aux;
-//        }
-//        else {
-//                if (a -> hizq == NULL) {
-//                    aux = a -> hder;
-//                    delete a;
-//                    a = aux;
-//                }
-//                    else {
-//                        a -> info = Minimo (a -> hder);
-//                        Borrar_Minimo (a -> hder);
-//                    }
-//        }
-//    }
-//    else {
-//            if (cedula < seleccionarCedulaPaciente(a -> info))   {
-//                eliminarConCedula (cedula, a -> hizq);
-//            }
-//            else  {
-//                eliminarConCedula (cedula, a -> hder);
-//            }
-//    }
+//     ABB aux;
+//     if (cedula == seleccionarCedulaPaciente(a -> info)) {
+//         if (a -> hder == NULL) {
+//             aux = a -> hizq;
+//             delete a;
+//             a = aux;
+//         }
+//         else {
+//                 if (a -> hizq == NULL) {
+//                     aux = a -> hder;
+//                     delete a;
+//                     a = aux;
+//                 }
+//                     else {
+//                         a -> info = Minimo (a -> hder);
+//                         Borrar_Minimo (a -> hder);
+//                     }
+//         }
+//     }
+//     else {
+//             if (cedula < seleccionarCedulaPaciente(a -> info))   {
+//                 eliminarConCedula (cedula, a -> hizq);
+//             }
+//             else  {
+//                 eliminarConCedula (cedula, a -> hder);
+//             }
+//     }
 //
-//}
+// }
 //
 
 //
 //
 //}
 //
-//void listarPacientesSinConsultas (ABB a) {
+// void listarPacientesSinConsultas (ABB a) {
 //
-//if (a !=NULL) {
+// if (a !=NULL) {
 //    if (seleccionarCantConsultas(a->info) == 0 ) {
 //        listarPacientesSinConsultas (a->hizq);
 //        desplegarPaciente (a->info);
@@ -186,9 +198,9 @@ void desplegarNodosArbolOrden (ArbolExpre a) {
 //
 //}
 //
-//void sumarConsultaAlABB (ABB &a, long int ced)
+// void sumarConsultaAlABB (ABB &a, long int ced)
 //{
-//if (a != NULL )
+// if (a != NULL )
 //{
 //    {
 //         if (seleccionarCedulaPaciente (a->info) == ced)
@@ -205,11 +217,11 @@ void desplegarNodosArbolOrden (ArbolExpre a) {
 //}
 //
 //
-//int obtenerPacientesConCantidadDeConsultas (ABB a, int CaConsultas) {
-//if (a == NULL) {
+// int obtenerPacientesConCantidadDeConsultas (ABB a, int CaConsultas) {
+// if (a == NULL) {
 //    return 0;
 //}
-//else {
+// else {
 //    if (CaConsultas <= seleccionarCantConsultas(a->info))
 //        {return 1+ obtenerPacientesConCantidadDeConsultas (a->hizq, CaConsultas) + obtenerPacientesConCantidadDeConsultas(a->hder, CaConsultas);}
 //    else {return obtenerPacientesConCantidadDeConsultas (a->hizq, CaConsultas) + obtenerPacientesConCantidadDeConsultas(a->hder, CaConsultas);}
@@ -217,3 +229,80 @@ void desplegarNodosArbolOrden (ArbolExpre a) {
 //
 //}
 //
+
+//------------------------PUSH DESDE VS CODE (ALE)----------------------------------------
+
+void insertarValorArbol(boolean valor, ArbolExpre &a) // ATOMIC
+{
+    if (a == NULL)
+    {
+        a = new NodoA;
+        a->info = valor;
+        a->hizq = NULL;
+        a->hder = NULL;
+    }
+}
+
+void copiarArbolAOtro(ArbolExpre ar, ArbolExpre &a)
+{
+}
+
+//  Minimo (ABB a) {
+//    if (a -> hizq == NULL)   {
+//            return (a->info);
+//    }
+//    else {
+//         return Minimo (a -> hizq); }
+
+//  Maximo (ABB a) {
+//    if (a -> hder == NULL)   {
+//            return (a->info);
+//    }
+//    else {
+//         return Maximo (a -> hder); }
+
+void compound()
+{
+    while (hizq != NULL)
+    {
+    }
+}
+
+void cargarParentesis(ArbolExpre &a, char parentesis)
+{
+    if (a == NULL)
+    {
+        a = new NodoA;
+        a->info = parentesis;
+        a->hizq = NULL;
+        a->hder = NULL;
+    }
+}
+
+void cargarOperadorNOT(ArbolExpre &a, ArbolExpre ar, char operador)
+{
+    // cargar un arbol en hder, en el min cargar parentesis (hizq)
+}
+
+void cargarOperadorAndOr(ArbolExpre &a, ArbolExpre ar, ArbolExpre arb, char operador) {}
+// ar copiar a otro arbol como hizq,
+// arb copiar a otro como hder
+
+boolean evaluarExpresion(ArbolExpre arbol)
+{
+    if (arbol->info == valor)
+    {
+        if (valor)
+        {
+            return true;
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
+void enumerarNodos(ArbolExpre &a) {}
+
+// void colocarParentesis(ArbolExpre &a) {}
