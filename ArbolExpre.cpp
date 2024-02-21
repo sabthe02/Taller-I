96017688bd3cc4c4ad31638fcc5413e2f04d127a
agtalvi <94943833+agtalvi@users.noreply.github.com>
2024-02-21 19:51:18 -0300

    cambios arbolExpre clase consulta

--- ArbolExpre.cpp
+++ ArbolExpre.cpp
@@ -100,151 +100,25 @@ void desplegarNodosArbolOrden(ArbolExpre a)
     }
 }
 
-// boolean existe (ABB a, long int cedula) {
-// if (a == NULL) {
-//     return FALSE;
-// }
-// else {
-//     if (cedula == seleccionarCedulaPaciente(a->info)) {
-//         return TRUE;
-//     }
-//     else {
-//             if (cedula < seleccionarCedulaPaciente(a->info)) {
-//                 return existe (a->hizq, cedula);
-//             }
-//             else {
-//                 return existe (a->hder, cedula);
-//             }
-//     }
-// }
-//
-//
-// }
-//
-// paciente Minimo (ABB a) {
-//     if (a -> hizq == NULL)   {
-//             return (a->info);
-//     }
-//     else {
-//          return Minimo (a -> hizq); }
-//
-//     }
-//
-// void Borrar_Minimo (ABB &a) {
-//     ABB aux;
-//     if (a -> hizq == NULL) {
-//             aux = a -> hder;
-//             delete a;
-//             a = aux;
-//     }
-//     else
+//-- -- -- -- -- -- -- -- -- -- -- --PUSH DESDE VS CODE(ALE)-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+// void insertarValorArbol(boolean valor, ArbolExpre &a) // ATOMIC
+// {
+//     if (a == NULL)
 //     {
-//         Borrar_Minimo (a -> hizq);
+//         a = new NodoA;
+//         a->info = valor;
+//         a->hizq = NULL;
+//         a->hder = NULL;
 //     }
 // }
-//
-// void eliminarConCedula (long int cedula, ABB &a) {
-//
-//     ABB aux;
-//     if (cedula == seleccionarCedulaPaciente(a -> info)) {
-//         if (a -> hder == NULL) {
-//             aux = a -> hizq;
-//             delete a;
-//             a = aux;
-//         }
-//         else {
-//                 if (a -> hizq == NULL) {
-//                     aux = a -> hder;
-//                     delete a;
-//                     a = aux;
-//                 }
-//                     else {
-//                         a -> info = Minimo (a -> hder);
-//                         Borrar_Minimo (a -> hder);
-//                     }
-//         }
-//     }
-//     else {
-//             if (cedula < seleccionarCedulaPaciente(a -> info))   {
-//                 eliminarConCedula (cedula, a -> hizq);
-//             }
-//             else  {
-//                 eliminarConCedula (cedula, a -> hder);
-//             }
-//     }
-//
-// }
-//
-
-//
-//
-//}
-//
-// void listarPacientesSinConsultas (ABB a) {
-//
-// if (a !=NULL) {
-//    if (seleccionarCantConsultas(a->info) == 0 ) {
-//        listarPacientesSinConsultas (a->hizq);
-//        desplegarPaciente (a->info);
-//        listarPacientesSinConsultas (a->hder);
-//    }
-//    else {
-//        listarPacientesSinConsultas (a->hizq);
-//        listarPacientesSinConsultas (a->hder);
-//    }
-//
-//}
-//
-//
-//}
-//
-// void sumarConsultaAlABB (ABB &a, long int ced)
-//{
-// if (a != NULL )
-//{
-//    {
-//         if (seleccionarCedulaPaciente (a->info) == ced)
-//             sumarConsulta (a->info);
-//         else
-//            {if (ced < seleccionarCedulaPaciente (a->info))
-//         sumarConsultaAlABB (a->hizq, ced);
-//         else sumarConsultaAlABB (a->hder, ced);
-//          }
-//    }
-//
-//
-//}
-//}
-//
-//
-// int obtenerPacientesConCantidadDeConsultas (ABB a, int CaConsultas) {
-// if (a == NULL) {
-//    return 0;
-//}
-// else {
-//    if (CaConsultas <= seleccionarCantConsultas(a->info))
-//        {return 1+ obtenerPacientesConCantidadDeConsultas (a->hizq, CaConsultas) + obtenerPacientesConCantidadDeConsultas(a->hder, CaConsultas);}
-//    else {return obtenerPacientesConCantidadDeConsultas (a->hizq, CaConsultas) + obtenerPacientesConCantidadDeConsultas(a->hder, CaConsultas);}
-//}
-//
-//}
-//
-
-//------------------------PUSH DESDE VS CODE (ALE)----------------------------------------
-
-void insertarValorArbol(boolean valor, ArbolExpre &a) // ATOMIC
-{
-    if (a == NULL)
-    {
-        a = new NodoA;
-        a->info = valor;
-        a->hizq = NULL;
-        a->hder = NULL;
-    }
-}
 
 void copiarArbolAOtro(ArbolExpre ar, ArbolExpre &a)
 {
+    // si ar != null, copiar nodo
+    // copiar info de ar->info a a->info
+    // llamar copiarArbolAOtro(ar->hizq, a->hizq)
+    // llamar copiarArbolAOtro(ar->hder, a->hder)
 }
 
 //  Minimo (ABB a) {
@@ -294,15 +168,26 @@ boolean evaluarExpresion(ArbolExpre arbol)
     {
         if (valor)
         {
-            return true;
+            return TRUE;
             else
             {
-                return false;
+                return FALSE;
             }
         }
-    }
+    } // si en raiz hay un boolean, entonces retorno valor del boolean. PASO BASE,
+    // sino, si en raiz hay un NOT, evaluo con ! y llamada recursivs a evaluarExpresion(arbol->hder)
+    // sino si en raiz hay AND u OR, llamada recursivs a evaluarExpresion(arbol->hizq), conecto con || u &&, y luego llamada recursiva a evaluar con evaluarExpresion(arbol->hder)
 }
 
-void enumerarNodos(ArbolExpre &a) {}
+// void enumerarNodos(ArbolExpre &a) {}
 
-// void colocarParentesis(ArbolExpre &a) {}
+void colocarParentesis(ArbolExpre &a)
+{
+    // colocar parentesis como hijo del ultimo hizq, y como hijo del ultompo hder
+    // mientras hizq sea  != null, avanzo, sigo yendo a la izquierda
+    // mientras hder sea  != null, avanzo, sigo yendo a la derecha
+}
+
+/*Para el compound, ejemplo COMPOUND 1 AND 2 
+Busco en la lista la expre1, copio su árbol, busco en la lista la expre2 y copio su arbol, creo un nuevo árbol cuya raíz sea el AND, el hizq es el árbol copiado de expre1, y el hder es el árbol copiado de expre2. Creo la raíz, y se le engancha el árbol de 
+/*
